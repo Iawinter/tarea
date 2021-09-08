96257bee144b0ab346ea8a7e20bedb8c28b33a48
iawinter <iawinter@uc.cl>
2021-09-08 17:34:34 -0300

    a

--- sitio/settings.py
+++ sitio/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.TemporadasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
