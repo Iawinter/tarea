87177a75ebe36a045d70613416d50dc5a6597698
iawinter <iawinter@uc.cl>
2021-09-08 17:32:04 -0300

    ahora

--- sitio/settings.py
+++ sitio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app.apps.TemporadasConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -71,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sitio.wsgi.application'
 
-
+TEMPLATE_DIRS = (os.path.join(BASE_DIR))
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
