a84d9cc50fe0bfd8f494db07e011cbb229424581
iawinter <iawinter@uc.cl>
2021-09-08 16:28:51 -0300

    app

--- app/views.py
+++ app/views.py
@@ -16,4 +16,4 @@ def index(request):
         if ep1["name"] not in lista:
             lista.append(ep1["name"])
     ciudades["cities"] = lista
-    return render(request, 'aplicacion/index.html', ciudades)
+    return render(request, 'app/index.html', ciudades)
