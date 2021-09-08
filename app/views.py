874c0ad5eaabd334b564092700e1610e207a5ec4
iawinter <iawinter@uc.cl>
2021-09-08 16:23:26 -0300

    requests

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from requests import get
 
+#def index(request):
+#   return HttpResponse("Hello, Antonia Vega.")
 
 def index(request):
-    return HttpResponse("Hello, Antonia Vega.")
-
+    ciudades= {"cities": [], "usuarios": []}
+    r = get("https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/cities")
+    r = r.json()
+    lista = []
+    for ep1 in r:
+        if ep1["name"] not in lista:
+            lista.append(ep1["name"])
+    ciudades["cities"] = lista
+    r = get("https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/usuarios")
+    r = r.json()
+    lista = []
+    for ep in r:
+        if ep["name"] not in lista:
+            lista.append(ep["name"])
+    ciudades["usuarios"] = lista
+    return render(request, 'aplicacion/index.html', ciudades)
