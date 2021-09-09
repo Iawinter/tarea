d315fbb5b0d89b2edb9cd532a1810dc2b8c0d8b7
iawinter <iawinter@uc.cl>
2021-09-09 02:12:27 -0300

    ciudades

--- app/views.py
+++ app/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.urls import reverse
 from requests import get
 
-#def index(request):
-#   return HttpResponse("Hello, Antonia Vega.")
-
 def index(request):
     ciudades= {"cities": []}
     r = get("https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/cities")
@@ -25,9 +22,9 @@ def index(request):
     ciudades["usuarios"] = lista
     return render(request, 'index.html', ciudades)
 
-def ciudades(request, id_ciudad):
-    r = get(f"https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/cities/{id_ciudad}")
+def ciudades(request):
+    r = get(f"https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/cities")
     r = r.json()
-    r = r[0]
+    #r = r[0]
     diccionario = {'nombre': r['name'], 'pais': r['country'], 'usuarios': r['users']}
     return render(request, 'ciudades.html', diccionario)
