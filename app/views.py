18b623b87b38650c68d3887cab5420f446f79937
iawinter <iawinter@uc.cl>
2021-09-08 23:58:19 -0300

    html2

--- app/views.py
+++ app/views.py
@@ -16,4 +16,12 @@ def index(request):
         if ep1["name"] not in lista:
             lista.append(ep1["name"])
     ciudades["cities"] = lista
+    r = get("https://us-central1-taller-integracion-310700.cloudfunctions.net/tarea-1-2021-2/26425/usuarios")
+    r = r.json()
+    lista = []
+    for ep in r:
+        if ep["name"] not in lista:
+            lista.append(ep["name"])
+    ciudades["usuarios"] = lista
     return render(request, 'index.html', ciudades)
+
