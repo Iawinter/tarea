b8c1f86844abe9676da967b5a9b3818278203b0e
iawinter <iawinter@uc.cl>
2021-09-09 22:58:23 -0300

    a

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('ciudades/<str:id_ciudad>/', views.ciudades, name='ciudades'),
     path('usuarios/<str:id_usuario>/', views.usuarios, name='usuarios'),
-    path('busqueda/', views.busqueda, name='busqueda'),
+    path('app/busqueda/', views.busqueda, name='busqueda'),
 ]
